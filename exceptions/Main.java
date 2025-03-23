27e950a2e13728b352132c140db25389138bc62f
5arSanti <santiari05@hotmail.com>
2025-03-23 00:26:55 -0500

    Feat: Update Main class to handle integer input and perform addition

--- exceptions/Main.java
+++ exceptions/Main.java
@@ -1,18 +1,21 @@
 package exceptions;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            
             Scanner sc = new Scanner(System.in);
-            System.out.print("Ingrese un número entero: ");
-            String input = sc.next();
-            int number = Integer.parseInt(input);
-            System.out.println("Número ingresado: " + number);
-        } catch (NumberFormatException e) {
-            System.out.println("Error: el valor ingresado no es un número entero.");
+            System.out.print("Ingrese el primer número: ");
+            int num1 = sc.nextInt();
+            System.out.print("Ingrese el segundo número: ");
+            int num2 = sc.nextInt();
+            int sum = num1 + num2;
+            System.out.println("La suma es: " + sum);
+            
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Debe ingresar un número entero.");
         }
     }
 }
-
